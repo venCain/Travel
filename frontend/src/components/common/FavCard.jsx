74cf57af06c3f6602b4838bb32b8e785774cf537
NavaneethSuresan <navaneeth@techis.io>
2022-11-08 12:53:50 +0530

    done

---
+++ frontend/src/components/common/FavCard.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+import Imgdislike from '../../assets/img/dislike.png'
+import Favourites from '../../containers/Favourites'
+
+import { useDispatch } from 'react-redux'
+import { deleteFavourite } from '../../reducks/favourites/operations'
+import { addPlaceAction } from '../../reducks/places/actions'
+import heart from '../../assets/img/heart.svg'
+import like from '../../assets/img/Like_button.svg'
+
+
+export const FavCard = ({favourite}) => {
+   const [showLikeButton,setShowLikeButton]=useState(true)
+    const dispatch =useDispatch()
+    return (<>
+  <div className="row">
+    <div className="image-container">
+      <img src={favourite.image} alt="" />
+    </div>
+    <div className="subrow">
+      <h4>{favourite.name}</h4>
+      <p>{favourite.description}</p>
+      {showLikeButton && (
+        <img
+          id="like"
+          src={like}
+          style={{backgroundColor:showLikeButton?'red':'white'}}
+          onClick={() => {
+            dispatch(deleteFavourite(favourite.id));
+          }}
+          alt=""
+        />)}
+       
+        
+      
+    </div>
+  </div>
+</>
+    );
+}
+export default FavCard
+
+
---
+++ frontend/src/components/common/Thumbnail.jsx
@@ -0,0 +1,74 @@
+import React, { useState, useEffect } from "react";
+import Imglike from "../../assets/img/Like_button.svg";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  addFavourite,
+  deleteFavourite,
+} from "../../reducks/favourites/operations";
+import { getFavourites } from "../../reducks/favourites/selectors";
+import heart from "../../assets/img/heart.svg";
+import { push } from "connected-react-router";
+import like from "../../assets/img/Like_button.svg";
+
+const Thumbnail = ({ place }) => {
+  const dispatch = useDispatch();
+  const clickFavourite = (place) => {
+    dispatch(addFavourite(place));
+  };
+  const selector = useSelector((state) => state);
+  const favourites = getFavourites(selector);
+  const [showLikeButton, setShowLikeButton] = useState(true);
+  useEffect(() => {
+    let favoritePlace = favourites.filter(
+      (favourite) => favourite.id == place.id
+    );
+    if (favoritePlace.length > 0) {
+      setShowLikeButton(false);
+    }
+  }, [favourites]);
+
+  return (
+    <>
+      <div class="row">
+        <div class="image-container">
+          <img
+            src={place.image}
+            alt=""
+            onClick={() => dispatch(push( "/places/"+place.id))}
+          />
+        </div>
+        <div className="subrow">
+          <h4>{place.name}</h4>
+          <p>{place.description}</p>
+          {/* {
+            <img
+              id="like"
+              src={Imglike}
+              onClick={() => {
+                setShowLikeButton(!showLikeButton);
+                if (showLikeButton) {
+                  clickFavourite(place);
+                } else {
+                  dispatch(deleteFavourite(place.id));
+                }
+              }}
+              style={{ backgroundColor: showLikeButton ? "white" : "red" }}
+              alt=""
+            />
+          } */}
+          {showLikeButton? <img src={like} onClick={()=>{
+            setShowLikeButton(!showLikeButton)
+            clickFavourite(place) 
+          }} id='like'/>:<img src={heart} onClick={()=>{
+            setShowLikeButton(!showLikeButton)
+            dispatch(deleteFavourite(place.id))
+          }} id='heart'/>}
+         
+          
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Thumbnail;
